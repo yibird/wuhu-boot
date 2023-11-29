7eaea391b476db4cbe4bf7210860fd0137337ba7
zchengfeng <2684837849@qq.com>
2023-11-29 19:25:16 +0800

    feat: 新增项目启动日志

--- wuhu-boot-server/src/main/java/com/fly/ServerApplication.java
+++ wuhu-boot-server/src/main/java/com/fly/ServerApplication.java
@@ -3,6 +3,7 @@ package com.fly;
 import jakarta.validation.constraints.NotNull;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,8 +12,11 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.context.WebServerInitializedEvent;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestClient;
+
+import java.lang.management.ManagementFactory;
 
 /**
  * @Description: 服务启动类
@@ -20,8 +24,17 @@ import org.springframework.web.client.RestClient;
  * @Data: 2022/12/6 14:24
  */
 @SpringBootApplication
-public class ServerApplication extends SpringBootServletInitializer implements ApplicationRunner, ApplicationListener<WebServerInitializedEvent> {
+public class ServerApplication extends SpringBootServletInitializer implements ApplicationRunner, ApplicationListener<ContextRefreshedEvent> {
     private static final Logger LOGGER = LogManager.getLogger();
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${spring.application.version}")
+    private String version;
+
+    @Value("${server.port}")
+    private int port;
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
@@ -33,23 +46,22 @@ public class ServerApplication extends SpringBootServletInitializer implements A
     }
 
     @Override
-    public void onApplicationEvent(@NotNull WebServerInitializedEvent event) {
-        String appName = this.getApplicationName(event);
-        int port = event.getWebServer().getPort();
-        LOGGER.info(appName + " start successfully \uD83C\uDF89,port:" + port);
-    }
-
-    private String getApplicationName(WebServerInitializedEvent event) {
-        Environment env = event.getApplicationContext().getEnvironment();
-        return env.getProperty("spring.application.name");
+    public void run(ApplicationArguments args) throws Exception {
+        RestClient.create().get().uri("http://localhost:9999/").retrieve().body(String.class);
     }
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-        RestClient.create()
-                .get()
-                .uri("http://localhost:9999/")
-                .retrieve()
-                .body(String.class);
+    public void onApplicationEvent(@NotNull ContextRefreshedEvent event) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(appName).append(" start successfully \uD83C\uDF89,");
+        sb.append(" Port: ").append(port);
+        sb.append(", Version: ").append(version);
+        sb.append(", Pid: ").append(getProcessId());
+        LOGGER.info(sb.toString());
+    }
+
+    private String getProcessId() {
+        return ManagementFactory.getRuntimeMXBean()
+                .getName().split("@")[0];
     }
 }
